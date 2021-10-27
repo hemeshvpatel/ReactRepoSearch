d068aaa7ead3d7cd995a6d75e5a1d3e91cca7259
hemesh <hemesh.v.patel@gmail.com>
2021-10-26 19:18:08 -0500

    styling refactoring, dropdown disabled when filtering enabled, small fixes

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,6 +3,8 @@ import styled, { css } from "styled-components";
 import SearchBar from "./SearchBar";
 import { GoMarkGithub as GithubIcon } from "react-icons/go";
 import { useHistory, withRouter } from "react-router-dom";
+import { FaUserSecret } from "react-icons/fa";
+import { DivFlexCenter } from "../globals/styles";
 
 const Container = styled.div`
   ${({ theme }) => css`
@@ -11,23 +13,31 @@ const Container = styled.div`
   `}
 `;
 
-const Wrapper = styled.div`
+const Wrapper = styled(DivFlexCenter)`
   height: 100vh;
-  display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
   gap: 1rem;
 `;
 
-const Title = styled.div`
+const Title = styled(DivFlexCenter)`
   ${({ theme }) => css`
     font-size: ${theme.fontSizes.title};
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    align-self: center;
     gap: 1rem;
+  `}
+`;
+
+const SecretLI = styled.a`
+  ${({ theme }) => css`
+    color: ${theme.colors.secondary};
+    font-size: 2rem;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    cursor: pointer;
+
+    &:hover {
+      color: ${theme.colors.primary};
+    }
   `}
 `;
 
@@ -40,6 +50,9 @@ function Landing() {
     history.push(path);
   };
 
+  //Hidden link to my LinkedIn
+  const LIurl = "https://www.linkedin.com/in/hemeshvpatel/";
+
   return (
     <Container id="Filters">
       <Wrapper>
@@ -47,6 +60,9 @@ function Landing() {
           <GithubIcon /> Github Repository Search
         </Title>
         <SearchBar placeholder="Search..." onSubmit={routeChange} />
+        <SecretLI href={LIurl} target="_blank" rel="noopener noreferrer">
+          <FaUserSecret />
+        </SecretLI>
       </Wrapper>
     </Container>
   );
